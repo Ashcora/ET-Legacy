f136b3bb8038a2b2f4d9020ce16596396071f584
Tobias S <58374721+Ashcora@users.noreply.github.com>
2023-08-08 10:32:28 +0200

    Update ETLegacy_Update.sh

--- ETLegacy_Update.sh
+++ ETLegacy_Update.sh
@@ -3,31 +3,29 @@
 # Installation path
 install_path='/usr/local/games/enemy-territory-legacy/etlegacy-x86_64'
 
-# Function to send an email using ssmtp (see ssmtp for more details)
+# Function to send an email using mail command
 send_email() {
     local subject="$1"
     local body="$2"
-    echo "Subject: $subject" >/tmp/email.txt
-    echo -e "$body" >>/tmp/email.txt
-    ssmtp YOURMAIL@HERE.ME </tmp/email.txt
+    echo "$body" | mail -s "$subject" YOURMAIL@HERE.ME
 }
 
-# Download the latest ETLegacy Binaries
+# Downloading the latest ETLegacy Binaries
 curl -L https://www.etlegacy.com/download/file/537 -o etlegacy.tar.gz
 
-# Prüfen, ob das Herunterladen erfolgreich war
+# Checking if the download was successful
 if [ $? -ne 0 ]; then
     send_email "ETLegacy Update Failed" "The download of ETLegacy binaries failed. Please check the logs for details."
     exit 1
 fi
 
-# Extracting the ETLegacy Binaries
+# Extracting the binaries
 tar xzf etlegacy.tar.gz -C "$install_path" --strip-components=1
 
-# Set permissions to installation path
+# Setting permissions
 chmod +x "$install_path"/*
 
-# Cleanup
+# Cleaning up
 rm etlegacy.tar.gz
 
 echo "ETLegacy has been updated!"
